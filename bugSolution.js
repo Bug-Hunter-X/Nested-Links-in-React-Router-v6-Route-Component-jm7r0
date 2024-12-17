210bcb39fc7a24d96cda63a5fd0b6a62bd9e3329
BugHunter-X <askzenai@gmail.com>
2024-12-17 01:16:08 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,43 @@
+import { BrowserRouter, Routes, Route, Link, useLocation } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link to="/about">Go to About</Link>
+    </div>
+  );
+}
+
+function About() {
+  const location = useLocation();
+  return (
+    <div>
+      <h1>About</h1>
+      <Link to="/">Go to Home</Link>
+      <p>Location: {location.pathname}</p>  
+    </div>
+  );
+}
+
+function NotFound() {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+    </div>
+  );
+}
+
+export default App;
